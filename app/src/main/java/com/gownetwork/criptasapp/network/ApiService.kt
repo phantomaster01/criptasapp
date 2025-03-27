b251866205b8ec884a927d1648ef5ca00d8bfadb
chan04luis <xorashk.l.angel@gmail.com>
2025-03-27 11:03:20 -0600

    Update ApiService.kt

--- app/src/main/java/com/gownetwork/criptasapp/network/ApiService.kt
+++ app/src/main/java/com/gownetwork/criptasapp/network/ApiService.kt
@@ -21,7 +21,7 @@ import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
-private const val BASE_URL = "https://gownetwork.dyndns.org:450"
+private const val BASE_URL = "https://gownetwork.dyndns.org:462"
 
 
 
